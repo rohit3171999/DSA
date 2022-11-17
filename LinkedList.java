da49434035b30ab8b1b152c93d8d5597a8771ab8
Rohit Sharma <rohit3171999@gmail.com>
2022-11-17 14:10:17 +0530

    formatted with Shfit+Alt+F

--- LinkedList.java
+++ LinkedList.java
@@ -1,61 +1,60 @@
-class LinkedList{
+class LinkedList {
     Node head;
-    class Node{
+
+    class Node {
         String data;
         Node next;
 
-        Node(String data){
-            this.data=data;
-            this.next=null;
+        Node(String data) {
+            this.data = data;
+            this.next = null;
         }
     }
 
-    //add first,, last
-    public void addFirst(String data){
-        Node newNode=new Node(data);
-        if(head==null){
-            head=newNode;
+    // add first,, last
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
             return;
 
         }
-        newNode.next=head;
-        head=newNode;
+        newNode.next = head;
+        head = newNode;
     }
-    public void addLast(String data){
-        Node newNode=new Node(data);
-        if(head==null){
-            head=newNode;
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
             return;
 
         }
-        Node currNode=head;
-        while(currNode.next !=null){
-            currNode=currNode.next;
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
 
         }
-        currNode.next=newNode;
-
+        currNode.next = newNode;
 
     }
-    //print
-    public void printList(){
-        if(head==null){
+
+    // print
+    public void printList() {
+        if (head == null) {
             System.out.println("list is empty");
             return;
         }
-        Node currNode=head;
-        while(currNode != null){
+        Node currNode = head;
+        while (currNode != null) {
             System.out.print(currNode.data + " -> ");
-            currNode=currNode.next;
+            currNode = currNode.next;
 
         }
         System.out.println("null");
         // currNode.next=newNode;
 
-
     }
-
-
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
@@ -64,7 +63,6 @@ class LinkedList{
         list.printList();
         list.addLast("last");
         list.printList();
-
 
     }
 }
